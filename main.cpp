bc4decd4bf8b88d51ecde3d26d3d75af313ca772
magalhaes1992-max <magalhaes1992@gmail.com>
2026-03-24 09:15:08 +0000

    teste

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 9; i++) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
     }
