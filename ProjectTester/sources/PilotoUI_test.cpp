2a7670b27248787c432376c8e00f23884985f7d2
nunoc-isep <1251610@isep.ipp.pt>
2026-05-12 09:51:07 +0100

    Pasta projectTester e inicio da programação do menu.

---
+++ ProjectTester/sources/PilotoUI_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void criarPiloto() {
+    cout<<"Hello World!"<<endl;
+}
+
+void listarPilotos() {
+    cout<<"Hello World!"<<endl;
+}
---
+++ ProjectTester/test_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "headers/Menu_test.h"
+
+using namespace std;
+
+int main() {
+    int opcao;
+    do {
+        menu(opcao);
+    }while (opcao != 6);
+}
